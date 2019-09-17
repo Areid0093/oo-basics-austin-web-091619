31437d226032b80b3c7b4690a0de65578d638699
Alec Reid <alecreid0093@gmail.com>
2019-09-17 22:11:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,9 +7,34 @@ class Book
   def title
     @title = title
   end
-
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wowm you read so fast!"
+  end
 end
 
 Book.new("And Then There Were None")
+book = Book.new("Some Title")
+book.turn_page
 
 
